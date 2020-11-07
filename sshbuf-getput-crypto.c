a15f7617f0c563e0e80bd429faed54ccc589b8ea
mirabilos <m@mirbsd.org>
2020-11-07 03:30:00 +0000

    more ECC

--- sshbuf-getput-crypto.c
+++ sshbuf-getput-crypto.c
@@ -49,76 +49,6 @@ sshbuf_get_bignum2(struct sshbuf *buf, BIGNUM **valp)
 	return 0;
 }
 
-static int
-get_ec(const u_char *d, size_t len, EC_POINT *v, const EC_GROUP *g)
-{
-	/* Refuse overlong bignums */
-	if (len == 0 || len > SSHBUF_MAX_ECPOINT)
-		return SSH_ERR_ECPOINT_TOO_LARGE;
-	/* Only handle uncompressed points */
-	if (*d != POINT_CONVERSION_UNCOMPRESSED)
-		return SSH_ERR_INVALID_FORMAT;
-	if (v != NULL && EC_POINT_oct2point(g, v, d, len, NULL) != 1)
-		return SSH_ERR_INVALID_FORMAT; /* XXX assumption */
-	return 0;
-}
-
-int
-sshbuf_get_ec(struct sshbuf *buf, EC_POINT *v, const EC_GROUP *g)
-{
-	const u_char *d;
-	size_t len;
-	int r;
-
-	if ((r = sshbuf_peek_string_direct(buf, &d, &len)) < 0)
-		return r;
-	if ((r = get_ec(d, len, v, g)) != 0)
-		return r;
-	/* Skip string */
-	if (sshbuf_get_string_direct(buf, NULL, NULL) != 0) {
-		/* Shouldn't happen */
-		SSHBUF_DBG(("SSH_ERR_INTERNAL_ERROR"));
-		SSHBUF_ABORT();
-		return SSH_ERR_INTERNAL_ERROR;
-	}
-	return 0;
-}
-
-int
-sshbuf_get_eckey(struct sshbuf *buf, EC_KEY *v)
-{
-	EC_POINT *pt = EC_POINT_new(EC_KEY_get0_group(v));
-	int r;
-	const u_char *d;
-	size_t len;
-
-	if (pt == NULL) {
-		SSHBUF_DBG(("SSH_ERR_ALLOC_FAIL"));
-		return SSH_ERR_ALLOC_FAIL;
-	}
-	if ((r = sshbuf_peek_string_direct(buf, &d, &len)) < 0) {
-		EC_POINT_free(pt);
-		return r;
-	}
-	if ((r = get_ec(d, len, pt, EC_KEY_get0_group(v))) != 0) {
-		EC_POINT_free(pt);
-		return r;
-	}
-	if (EC_KEY_set_public_key(v, pt) != 1) {
-		EC_POINT_free(pt);
-		return SSH_ERR_ALLOC_FAIL; /* XXX assumption */
-	}
-	EC_POINT_free(pt);
-	/* Skip string */
-	if (sshbuf_get_string_direct(buf, NULL, NULL) != 0) {
-		/* Shouldn't happen */
-		SSHBUF_DBG(("SSH_ERR_INTERNAL_ERROR"));
-		SSHBUF_ABORT();
-		return SSH_ERR_INTERNAL_ERROR;
-	}
-	return 0;	
-}
-
 int
 sshbuf_put_bignum2(struct sshbuf *buf, const BIGNUM *v)
 {
@@ -140,31 +70,3 @@ sshbuf_put_bignum2(struct sshbuf *buf, const BIGNUM *v)
 	explicit_bzero(d, sizeof(d));
 	return 0;
 }
-
-int
-sshbuf_put_ec(struct sshbuf *buf, const EC_POINT *v, const EC_GROUP *g)
-{
-	u_char d[SSHBUF_MAX_ECPOINT];
-	size_t len;
-	int ret;
-
-	if ((len = EC_POINT_point2oct(g, v, POINT_CONVERSION_UNCOMPRESSED,
-	    NULL, 0, NULL)) > SSHBUF_MAX_ECPOINT) {
-		return SSH_ERR_INVALID_ARGUMENT;
-	}
-	if (EC_POINT_point2oct(g, v, POINT_CONVERSION_UNCOMPRESSED,
-	    d, len, NULL) != len) {
-		return SSH_ERR_INTERNAL_ERROR; /* Shouldn't happen */
-	}
-	ret = sshbuf_put_string(buf, d, len);
-	explicit_bzero(d, len);
-	return ret;
-}
-
-int
-sshbuf_put_eckey(struct sshbuf *buf, const EC_KEY *v)
-{
-	return sshbuf_put_ec(buf, EC_KEY_get0_public_key(v),
-	    EC_KEY_get0_group(v));
-}
-
